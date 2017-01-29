c345c336882bf6b5a6b5b9dc2a4a17c307b8148c
Pavel Tereschenko <me@pavlik.pro>
2017-01-29 19:42:35 +0300

    РЕализовано хранение стейта в localStorage

--- src/front/index.js
+++ src/front/index.js
@@ -7,8 +7,23 @@ import App from './containers/App';
 import './style.less';
 import './inject';
 
-const store = createStore(app);
+const getLocalState = (Component) => {
+  try {
+    const state = localStorage.getItem('state');
+
+    if (state) return createStore(Component, JSON.parse(state));
+    throw new Error();
+  } catch (e) {
+    return createStore(Component);
+  }
+};
+
+const store = getLocalState(app);
 const render = createRender('#app-root')(store)(App);
+
+store.subscribe(() => {
+  localStorage.setItem('state', JSON.stringify(store.getState()));
+});
 
 if (document.readyState !== 'loading') render();
 else document.addEventListener('DOMContentLoaded', render);
